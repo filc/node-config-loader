c24a62accde69c0f9e96bd08b2bc8c334a8696ce
Peter Ujvari <filc@Peters-MacBook-Pro-4.local>
2015-11-04 13:48:29 +0100

    config loader refact

--- config-loader.js
+++ config-loader.js
@@ -3,15 +3,13 @@ _.mixin(require('underscore.nested'));
 
 function loadConfig(configFile, app, env) {
     var config = require(configFile);
-    var commonAppCommon, commonAppEnv, appCommon, appEnv;
 
-    commonAppCommon = _.clone(_.getNested(config, 'common.common')) || {};
-    commonAppEnv = env ? _.clone(_.getNested(config, 'common.' + env, {})) : {};
-
-    appCommon = app ? _.clone(_.getNested(config, app + '.common', {})) : {};
-    appEnv = app && env ? _.clone(_.getNested(config, app + '.' + env, {})) : {};
-
-    return _.extend(commonAppCommon, commonAppEnv, appCommon, appEnv);
+    return _.extend(
+        _.clone(_.getNested(config, 'common.common', {})),
+        _.clone(_.getNested(config, 'common.' + env, {})),
+        _.clone(_.getNested(config, app + '.common', {})),
+        _.clone(_.getNested(config, app + '.' + env, {}))
+    );
 }
 
 module.exports.loadConfig = loadConfig;
